047cbfa9650c4728d3fd679f9b28fe8fe5941d0e
vortep.7 <0ndreyp7777@gmail.com>
2024-02-27 09:12:24 +0300

    Initial Commit

---
+++ PlovProject(Pet-project)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlovProject(Pet-project)
+//
+//  Created by Андрей Петров on 27.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PlovProject(Pet-project)UITests/PlovProject_Pet_project_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PlovProject_Pet_project_UITestsLaunchTests.swift
+//  PlovProject(Pet-project)UITests
+//
+//  Created by Андрей Петров on 27.02.2024.
+//
+
+import XCTest
+
+final class PlovProject_Pet_project_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
